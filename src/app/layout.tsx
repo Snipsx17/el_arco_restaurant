3539efc2a88b5129801e38a58588cf031db23270
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-02-10 14:48:37 -0500

    :tada: First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "El Arco Restaurante",
+  description: "Restaurante acogedor con una cocina llena de sabores frescos, con magnifica terraza con vistas a la plaza del Casco Antiguo de Calpe. Una combinación encantadora para pasar las tardes de verano en un ambiente tranquilo y agradable.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello word </h1>
+    </main>
+  );
+}
