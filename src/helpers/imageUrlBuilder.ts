8969f55f84b502a4ba9469ec2f95eb3656b36fea
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-02-12 16:51:55 -0500

    feat: added helper to build image url from sanity

---
+++ src/helpers/imageUrlBuilder.ts
@@ -0,0 +1,12 @@
+import { client } from "@/sanity/lib/client";
+import {
+  createImageUrlBuilder,
+  type SanityImageSource,
+} from "@sanity/image-url";
+
+const { projectId, dataset } = client.config();
+
+export const urlFor = (source: SanityImageSource) =>
+  projectId && dataset
+    ? createImageUrlBuilder({ projectId, dataset }).image(source)
+    : null;
