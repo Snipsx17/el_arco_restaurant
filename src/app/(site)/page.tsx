9e3bec21e096dde3027390140c88fe88c6879805
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-03-13 15:16:33 -0400

    feat: added about section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,3 +1,5 @@
+
+import About from "@/components/sections/About";
 import Hero from "@/components/sections/Hero";
 import { urlFor } from "@/helpers/imageUrlBuilder";
 import { client } from "@/sanity/lib/client";
@@ -15,22 +17,26 @@ export default async function Home() {
   if (!page?.sections) return null
 
   return (page.sections.map(section => {
-    switch (section._type) {
+      switch (section._type) {
       case "heroSection":
-        const imageUrl = section.image
-          ? urlFor(section.image)?.width(1920).quality(80).url()
-          : null
-        return (
-          <Hero
-            key={section._key}
-            data={{ ...section, image :imageUrl }}
-          />
-        )
+      const imageUrl = section.image
+      ? urlFor(section.image)?.width(1920).quality(80).url()
+      : null
+      return (
+      <Hero
+        key={section._key}
+        data={{ ...section, image: imageUrl }}
+      />
+      )
+      case "aboutSection":
+      const aboutImageUrl = section.image
+      ? urlFor(section.image)?.width(800).quality(80).url()
+      : null
+      return (
+      <About key={section._key} data={{ ...section, image: aboutImageUrl }} />
+      )
       default:
-        return null
+      return null
     }
-  }
-  
-)
-)
+  }))
 }
