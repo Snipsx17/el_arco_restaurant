e6654b6ac86b45a791fcfa6c88da3ae560cf8fd4
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-03-13 15:17:24 -0400

    feat: added data-aos properties to hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -8,7 +8,6 @@ interface Props {
 
 export default function Hero({ data }: Props) {
   const { title, subtitle, text, image, buttons } = data
-  console.log(image);
   return (
     <section className={`h-screen flex justify-center content-center items-center flex-col gap-9 bg-no-repeat bg-cover px-6 md:px-0`} style={{ backgroundImage: image ? `url(${image})` : undefined }}>
 
@@ -18,9 +17,9 @@ export default function Hero({ data }: Props) {
 
       <Separator />
       <div className="text-center z-10">
-        <h1 className="text-5xl md:text-7xl capitalize mb-4 animate__animated animate__fadeInUp">{title}</h1>
-        <p className="text-2xl md:text-3xl mb-4 animate__animated animate__fadeInUp">{subtitle}</p>
-        {text && <p className="text-xl animate__animated animate__fadeInUp">{text}</p>}
+        <h1 className="text-5xl md:text-7xl capitalize mb-4" data-aos="fade-in" >{title}</h1>
+        <p className="text-2xl md:text-3xl mb-4" data-aos="fade-in" >{subtitle}</p>
+        {text && <p className="text-xl" data-aos="fade-in" >{text}</p>}
       </div>
       <Separator />
       <div className="z-10 flex gap-4 w-80">
@@ -28,7 +27,7 @@ export default function Hero({ data }: Props) {
           buttons?.map(button => (
 
             <Button key={button.label} size={'lg'} asChild variant={button.isPrimary ? 'customPrimary' : 'customSecondary'}>
-              <a href={button.url}>{button.label}</a>
+              <a href={button.url} target="_blank">{button.label}</a>
             </Button>
 
           ))
