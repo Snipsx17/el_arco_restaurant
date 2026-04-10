e81076db0d1c2db3171d68430cf50209b93a3ba5
Uberth Hernandez <ing.uberthhernandez@gmail.com>
2026-04-10 16:45:47 -0400

    fix: global layout

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,30 +1,10 @@
-import { Fraunces, Khula, Roboto } from 'next/font/google';
+
 import "../globals.css";
 import { NAVBAR_QUERY } from '@/sanity/queries';
 import { client } from '@/sanity/lib/client';
 import type { Navbar as NavbarType } from '@/sanity/types'
 import Navbar from '@/components/Navbar';
 import AOSInit from '@/components/AOSInit';
-
-const fraunces = Fraunces({
-    subsets: ['latin'],
-    display: 'swap',
-    variable: '--font-fraunces'
-})
-
-const khula = Khula({
-    subsets: ['latin'],
-    display: 'swap',
-    weight: '400',
-    variable: '--font-khula'
-})
-
-const roboto = Roboto({
-    subsets: ['latin'],
-    display: 'swap',
-    weight: '400',
-    variable: '--font-roboto'
-})
 
 export default async function HomeLayout({
     children,
@@ -40,14 +20,10 @@ export default async function HomeLayout({
     return (
         <>
             <AOSInit />
-            <body
-                className={`${fraunces.variable} ${khula.variable} ${roboto.variable} antialiased`}
-            >
                 <Navbar data={navbarData} />
                 <main>
                     {children}
                 </main>
-            </body>
         </>
     );
 }
